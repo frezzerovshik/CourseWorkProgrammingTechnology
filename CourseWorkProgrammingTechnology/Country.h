2f1d2681091d3b47d7808bdcf59fe1e11091f8d8
Artem Sharapov <frezzy12349@gmail.com>
2020-03-11 00:01:48 +0300

    New Types

---
+++ CourseWorkProgrammingTechnology/Country.h
@@ -0,0 +1,17 @@
+//
+//  Types.h
+//  CourseWorkProgrammingTechnology
+//
+//  Created by Артем Шарапов on 10.03.2020.
+//  Copyright © 2020 Hooba-booba. All rights reserved.
+//
+
+#pragma once
+
+enum class Country {
+    Russia,
+    Canada,
+    France,
+    Finland,
+    Japan
+};
