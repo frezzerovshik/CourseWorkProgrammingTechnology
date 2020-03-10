4024b8ce6e4a4b0d6b862612fdc4707591eb3b8d
Artem Sharapov <frezzy12349@gmail.com>
2020-03-10 23:52:13 +0300

    Initial commit

---
+++ CourseWorkProgrammingTechnology/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CourseWorkProgrammingTechnology
+//
+//  Created by Артем Шарапов on 10.03.2020.
+//  Copyright © 2020 Hooba-booba. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
